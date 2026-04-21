4256ab411c6225ded1f68e31b70a800e24250ed2
s.kudriavtsev <s.kudriavtsev@reglab.ru>
2026-04-21 10:15:54 +0500

    Добавлен контейнер

--- app/allocator/src/allocator.cpp
+++ app/allocator/src/allocator.cpp
@@ -18,14 +18,18 @@ constexpr long long factorial(int n)
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
 {
+    // создание экземпляра std::map<int, int>
     std::map<int, int> defaultMap_;
 
+    // заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
     for (int i = 0; i < 10; ++i) {
         defaultMap_[i] = static_cast<int>(factorial(i));
     }
 
+    // создание экземпляра std::map<int, int> с новым аллокатором, ограниченным 10 элементами
     std::map<int, int, std::less<>, MyAllocator<std::pair<const int, int>, 10>> customMap_;
 
+    // заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
     try {
         for (int i = 0; i < 10; ++i) {
             customMap_[i] = static_cast<int>(factorial(i));
@@ -34,10 +38,35 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[])
         std::cout << "too much elems" << std::endl;
     }
 
+    // вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
     for (auto & elem : customMap_) {
         std::cout << elem.first << " " << elem.second << std::endl;
     }
 
-//    std::cout << customMap_.get_allocator().state_->currentNum_ << " " << std::endl;
+    // создание экземпляра своего контейнера для хранения значений типа int
+    MyContainer<int> defaultAllocContainer_;
+
+    // заполнение 10 элементами от 0 до 9
+    for (int i = 0; i < 10; ++i) {
+        defaultAllocContainer_.push_front(i);
+    }
+
+    // создание экземпляра своего контейнера для хранения значений типа int с новым аллокатором, ограниченным 10 элементами
+    MyContainer<int, MyAllocator<int, 10>> customAllocContainer_;
+
+    // заполнение 10 элементами от 0 до 9
+    try {
+        for (int i = 0; i < 10; ++i) {
+            customAllocContainer_.push_front(i);
+        }
+    } catch (const std::bad_alloc&) {
+        std::cout << "too much elems" << std::endl;
+    }
+
+    // вывод на экран всех значений, хранящихся в контейнере
+    for (auto & elem : customAllocContainer_) {
+        std::cout << elem << std::endl;
+    }
+
     return 0;
 }
--- lib/allocator/inc/allocator_lib.hpp
+++ lib/allocator/inc/allocator_lib.hpp
@@ -13,46 +13,112 @@ struct MyAllocatorState {
     std::size_t currentNum_ = 0;
 };
 
-template <typename T, std::size_t NMax>
+template<typename T, std::size_t NMax>
 struct MyAllocator {
     using value_type = T;
 
-    template <typename U, std::size_t M> friend struct MyAllocator;
+    template<typename U, std::size_t M> friend
+    struct MyAllocator;
 
     std::shared_ptr<MyAllocatorState> state_;
 
     MyAllocator() : state_(std::make_shared<MyAllocatorState>()) {}
 
-    template <typename U>
-    MyAllocator(const MyAllocator<U, NMax>& other) noexcept
+    template<typename U>
+    MyAllocator(const MyAllocator<U, NMax> &other) noexcept
             : state_(other.state_) {}
 
-    template <typename U>
+    template<typename U>
     struct rebind {
         using other = MyAllocator<U, NMax>;
     };
 
-    template <typename U>
-    bool operator==(const MyAllocator<U, NMax>& other) const noexcept {
+    template<typename U>
+    bool operator==(const MyAllocator<U, NMax> &other) const noexcept {
         return state_ == other.state_;
     }
-    template <typename U>
-    bool operator!=(const MyAllocator<U, NMax>& other) const noexcept {
+
+    template<typename U>
+    bool operator!=(const MyAllocator<U, NMax> &other) const noexcept {
         return *this != other;
     }
 
-    value_type* allocate(std::size_t n) {
+    value_type *allocate(std::size_t n) {
         if (state_->currentNum_ + n > NMax) {
             throw std::bad_alloc();
         }
-        T* p = std::allocator<T>{}.allocate(n);
+        T *p = std::allocator<T>{}.allocate(n);
         state_->currentNum_ += n;
         return p;
     }
 
-    void deallocate(value_type* p, std::size_t n) noexcept {
+    void deallocate(value_type *p, std::size_t n) noexcept {
         std::allocator<T>{}.deallocate(p, n);
         state_->currentNum_ -= n;
+    }
+};
+
+template<typename T, typename Allocator = std::allocator<T>>
+class MyContainer {
+    struct Node {
+        T value;
+        Node *next;
+    };
+
+    using NodeAlloc = typename std::allocator_traits<Allocator>::template rebind_alloc<Node>;
+    using AllocTraits = std::allocator_traits<NodeAlloc>;
+
+    NodeAlloc alloc_;
+    Node *head_ = nullptr;
+
+public:
+    MyContainer() = default;
+
+    ~MyContainer() {
+        while (head_) {
+            Node *next = head_->next;
+            AllocTraits::destroy(alloc_, head_);
+            AllocTraits::deallocate(alloc_, head_, 1);
+            head_ = next;
+        }
+    }
+
+    class Iterator {
+        Node *current_;
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = T *;
+        using reference = T &;
+
+        Iterator(Node *node) : current_(node) {}
+
+        reference operator*() const { return current_->value; }
+        pointer operator->() { return &(current_->value); }
+
+        Iterator &operator++() {
+            if (current_) current_ = current_->next;
+            return *this;
+        }
+
+        Iterator operator++(int) {
+            Iterator temp = *this;
+            ++(*this);
+            return temp;
+        }
+
+        bool operator==(const Iterator &other) const { return current_ == other.current_; }
+        bool operator!=(const Iterator &other) const { return current_ != other.current_; }
+    };
+
+    Iterator begin() { return Iterator(head_); }
+    Iterator end() { return Iterator(nullptr); }
+
+    void push_front(const T &value) {
+        Node *newNode = AllocTraits::allocate(alloc_, 1);
+        AllocTraits::construct(alloc_, newNode, Node{value, head_});
+        head_ = newNode;
     }
 };
 
